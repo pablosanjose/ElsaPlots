ed4e8aa27741bdadde924a914372a042c8835312
Pablo San-Jose <lekand@gmail.com>
2018-10-31 16:23:26 +0100

    missing Point3D

--- src/plotlattice.jl
+++ src/plotlattice.jl
@@ -8,7 +8,7 @@ function plot(lat::Lattice; resolution = (1024, 1024), kw...)
 
     plot = plot!(scene, lat)
 
-    scale!(scene)
+    # scale!(scene)
 
     b1, b2 = boundingboxlat(lat)
     lookat = Vec3D((b1 + b2)/2)
@@ -22,9 +22,9 @@ end
 function default_theme(scene::SceneLike, ::Type{<: Plot(Lattice)})
     Theme(
         allintra = false, allcells = true, intralinks = true, interlinks = true,
-        shaded = false, dimming = 0.75, 
-        siteradius = 0.05, siteborder = 8, siteborderdarken = 1.0,
-        linkthickness = 20, linkoffset = 0.99, linkradius = 0.1,
+        shaded = true, dimming = 0.75, 
+        siteradius = 0.00, siteborder = 8, siteborderdarken = 1.0,
+        linkthickness = 20, linkoffset = 0.99, linkradius = 0.005,
         colorscheme = map(t -> RGBAf0(t...), ((0.410,0.067,0.031),(0.860,0.400,0.027),(0.940,0.780,0.000),(0.640,0.760,0.900),(0.310,0.370,0.650),(0.600,0.550,0.810),(0.150,0.051,0.100),(0.870,0.530,0.640),(0.720,0.130,0.250)))
         )
 end
@@ -91,7 +91,7 @@ end
 function drawlinks_hi!(plot, rdr, celldist, (col1, col2))
     isempty(rdr) && return nothing
     # positions = view(rdr, :, 1)
-    positions = [Point3f0(celldist + r) for (r, _) in rdr]
+    positions = [Point3D(celldist + r) for (r, _) in rdr]
     segments = [halfsegment(r, dr, 0) for (r, dr) in rdr]
     scales = [Vec3f0(plot[:linkradius][], plot[:linkradius][], norm(dr)) for dr in segments]
     cylinder = GLNormalMesh(Makie.Cylinder{3, Float32}(Point3f0(0., 0., 0.), Point3f0(0., 0, 1.), Float32(1)), 12)
