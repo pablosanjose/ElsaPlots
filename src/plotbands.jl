a7194c834901978dd52fb3e31f9b881175b97dce
Pablo San-Jose <lekand@gmail.com>
2018-11-05 22:01:27 +0100

    subband plotting

--- src/plotbands.jl
+++ src/plotbands.jl
@@ -22,10 +22,10 @@ function AbstractPlotting.plot!(plot::Plot(BrillouinMesh))
     #     sites = [Point3D(s) + Point3D(SVector(0,0,0.1*i)) for s in bm.mesh.sublats[1].sites]
     #     mesh!(plot, sites, [e[j] for j in 1:3], color = :white, strokewidth = 8)
     # end
-    mesh!(plot, Point3D.(bm.mesh.sublats[1].sites), [e[j] for e in bm.elements.indices, j in 1:3], color = :white, strokewidth = 8)
+    mesh!(plot, Point3D.(bm.mesh.lattice.sublats[1].sites), [e[j] for e in bm.mesh.elements.intra, j in 1:3], color = :white, strokewidth = 8)
     plot!(plot, bm.mesh)
 
-    sites = bm.mesh.sublats[1].sites
+    sites = bm.mesh.lattice.sublats[1].sites
     centers = [padright((sites[e[1]]+ sites[e[2]] + sites[e[3]])/3, Val(3)) for e in bm.elements.indices]
     normals = [cross(padright(sites[e[2]] - sites[e[1]], Val(3)), 
                      padright(sites[e[3]] - sites[e[1]], Val(3))) for e in bm.elements.indices]
@@ -49,16 +49,21 @@ end
 
 function default_theme(scene::SceneLike, ::Type{<: Plot(Bandstructure)})
     Theme(
-        wireframe = 0
+        wireframe = 0,
+        colorscheme = map(t -> RGBAf0(t...), ((0.410,0.067,0.031),(0.860,0.400,0.027),(0.940,0.780,0.000),(0.640,0.760,0.900),(0.310,0.370,0.650),(0.600,0.550,0.810),(0.150,0.051,0.100),(0.870,0.530,0.640),(0.720,0.130,0.250)))
         )
 end
 
 function AbstractPlotting.plot!(plot::Plot(Bandstructure))
     bs = to_value(plot[1])
+    subbands = QBox.gather(bs.mesh.elements.intra)
+    colors = collect(take(cycle(plot[:colorscheme][]), length(subbands)))
     
     sites = Point3D.(bs.mesh.lattice.sublats[1].sites)
-    mesh!(plot, sites, [e[j] for e in bs.mesh.elements.intra, j in 1:3], color = :white, strokewidth = 8)
-    plot!(plot, bs.mesh.lattice)
+    for (i, subband) in enumerate(subbands)
+        mesh!(plot, sites, [e[j] for e in subband, j in 1:3], color = colors[i], strokewidth = 8)
+    end
+    # plot!(plot, bs.mesh.lattice)
 
     # sites = bs.mesh.sublats[1].sites
     # centers = [(sites[e[1]]+ sites[e[2]] + sites[e[3]])/3 for e in bs.elements.indices]
