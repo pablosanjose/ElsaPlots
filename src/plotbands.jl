1114d3baddf73170696aa5376b5fe6d9b7913e24
Pablo San-Jose <lekand@gmail.com>
2018-11-06 22:13:54 +0100

    WIP

--- src/plotbands.jl
+++ src/plotbands.jl
@@ -22,16 +22,17 @@ function AbstractPlotting.plot!(plot::Plot(BrillouinMesh))
     #     sites = [Point3D(s) + Point3D(SVector(0,0,0.1*i)) for s in bm.mesh.sublats[1].sites]
     #     mesh!(plot, sites, [e[j] for j in 1:3], color = :white, strokewidth = 8)
     # end
-    mesh!(plot, Point3D.(bm.mesh.lattice.sublats[1].sites), [e[j] for e in bm.mesh.elements.intra, j in 1:3], color = :white, strokewidth = 8)
-    plot!(plot, bm.mesh)
-
     sites = bm.mesh.lattice.sublats[1].sites
-    centers = [padright((sites[e[1]]+ sites[e[2]] + sites[e[3]])/3, Val(3)) for e in bm.elements.indices]
-    normals = [cross(padright(sites[e[2]] - sites[e[1]], Val(3)), 
-                     padright(sites[e[3]] - sites[e[1]], Val(3))) for e in bm.elements.indices]
-    segments = [Point3D(c) => Point3D(c + 1 * n) for (c,n) in zip(centers, normals)]
-    linesegments!(plot, segments, color = :blue, strokewidth = 8)
+    for group in bm.mesh.elements.groupsintra
+        mesh!(plot, Point3D.(bm.mesh.lattice.sublats[1].sites), [e[j] for e in group, j in 1:3], color = :white, strokewidth = 8)
+        # plot!(plot, bm.mesh)
 
+        centers = [padright((sites[e[1]]+ sites[e[2]] + sites[e[3]])/3, Val(3)) for e in group]
+        normals = [cross(padright(sites[e[2]] - sites[e[1]], Val(3)), 
+                        padright(sites[e[3]] - sites[e[1]], Val(3))) for e in group]
+        segments = [Point3D(c) => Point3D(c + 60 * n) for (c,n) in zip(centers, normals)]
+        linesegments!(plot, segments, color = :blue, strokewidth = 8)
+    end
     return plot
  end
 
@@ -56,20 +57,35 @@ end
 
 function AbstractPlotting.plot!(plot::Plot(Bandstructure))
     bs = to_value(plot[1])
-    subbands = QBox.gather(bs.mesh.elements.intra)
-    colors = collect(take(cycle(plot[:colorscheme][]), length(subbands)))
+
+    nsubbands = QBox.ngroupsintra(bs.mesh.elements)
+    colors = collect(take(cycle(plot[:colorscheme][]), nsubbands))
     
     sites = Point3D.(bs.mesh.lattice.sublats[1].sites)
-    for (i, subband) in enumerate(subbands)
-        mesh!(plot, sites, [e[j] for e in subband, j in 1:3], color = colors[i], strokewidth = 8)
+    for i in 1:nsubbands
+    # i=1
+        group = bs.mesh.elements.groupsintra[i]
+        mesh!(plot, sites, [e[j] for e in group, j in 1:3], color = colors[i], strokewidth = 8)
     end
+
+    # nsubbands = QBox.ngroups(bs.mesh.elements)
+    # colors = collect(take(cycle(plot[:colorscheme][]), nsubbands))
+    
+    # sites = Point3D.(bs.mesh.lattice.sublats[1].sites)
+    # for (i, subband) in enumerate(bs.mesh.elements.groups)
+    #     mesh!(plot, sites, [e[j] for e in subband, j in 1:3], color = colors[i], strokewidth = 8)
+    # end
+
+    # @show typeof(plot)
     # plot!(plot, bs.mesh.lattice)
 
-    # sites = bs.mesh.sublats[1].sites
-    # centers = [(sites[e[1]]+ sites[e[2]] + sites[e[3]])/3 for e in bs.elements.indices]
-    # normals = [cross(sites[e[2]] - sites[e[1]], sites[e[3]] - sites[e[1]]) for e in bs.elements.indices]
-    # segments = [Point3D(c) => Point3D(c + 100 * n) for (c,n) in zip(centers, normals)]
-    # linesegments!(plot, segments, color = :blue, strokewidth = 8)
+    # sites = bs.mesh.lattice.sublats[1].sites
+    # for group in bs.mesh.elements.groupsintra
+    #     centers = [(sites[e[1]]+ sites[e[2]] + sites[e[3]])/3 for e in group]
+    #     normals = [cross(sites[e[2]] - sites[e[1]], sites[e[3]] - sites[e[1]]) for e in group]
+    #     segments = [Point3D(c) => Point3D(c + 60 * n) for (c,n) in zip(centers, normals)]
+    #     linesegments!(plot, segments, color = :blue, strokewidth = 8)
+    # end
 
     return plot
  end
